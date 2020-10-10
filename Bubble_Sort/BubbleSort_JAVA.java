bd123c0b9e4996827b3bb302d420a6a52b32548b
RiddhamGadia <42893286+RiddhamGadia@users.noreply.github.com>
2020-10-10 18:11:31 +0530

    Update bubble_sort.java

--- Bubble_Sort/BubbleSort_JAVA.java
+++ Bubble_Sort/BubbleSort_JAVA.java
@@ -1,9 +1,6 @@
-public class Solution {              //Name of the class is Solution and it is public
+class Solution {              //Name of the class is Solution and it is public
   public static void main(String[] args) {
       int arr[]={8,7,6,5,4,3,2};//Storing array elements
-      System.out.println("Before sorting");
-      for(int i=0;i<arr.length;i++)
-      System.out.print(arr[i]+" ");
       bubble_sort_algo(arr);
       System.out.println("\nAfter sorting");
       for(int i=0;i<arr.length;i++)
@@ -12,7 +9,6 @@ public class Solution {              //Name of the class is Solution and it is p
   }  
   static void bubble_sort_algo(int arr[])//Implementing bubble sort algorithm
   {
-      int flag=0;
       for(int i=0;i<arr.length-1;i++)
       {
           for(int j=0;j<arr.length-1-i;j++)
@@ -22,11 +18,8 @@ public class Solution {              //Name of the class is Solution and it is p
                   int swap=arr[j];     //Swapping the positions of elements
                   arr[j]=arr[j+1];
                   arr[j+1]=swap;
-                  flag=1;
               }
           }
-          if(flag==0)                //If array is sorted no need to sort so break
-          break;
       }
   }
 }
