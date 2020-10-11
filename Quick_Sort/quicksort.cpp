edb7a2854367c6036221a32e0417d34b24e286b5
haripatelll <34925349+haripatelll@users.noreply.github.com>
2020-10-10 23:15:08 -0400

    Update quicksort.cpp

--- Quick_Sort/quicksort.cpp
+++ Quick_Sort/quicksort.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 // We Partition the array by using the last element as the pivot
+// partition the array using last element as pivot
 int partition (int arr[], int low, int high) { 
     int pivot = arr[high];
     int i = (low - 1);   
     for (int j = low; j <= high- 1; j++) { 
+        //if current element is smaller than pivot, increment the low element
+        //swap elements at i and j
         if (arr[j] <= pivot) { 
-            i++;
+            i++;    // increment index of smaller element 
             swap(arr[i], arr[j]); 
         } 
     } 
@@ -20,7 +23,8 @@ void quickSort(int arr[], int low, int high)
 { 
     if (low < high) { 
         //partition the array 
-        int pivot = partition(arr, low, high);    
+        int pivot = partition(arr, low, high); 
+   
         //sort the sub arrays independently 
         quickSort(arr, low, pivot - 1); 
         quickSort(arr, pivot + 1, high); 
